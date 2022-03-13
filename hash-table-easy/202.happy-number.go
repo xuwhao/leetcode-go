249feff54c37a06a6accb8c8e67538a9fac4d574
xuwhao <63779478@qq.com>
2022-03-13 16:58:48 +0800

    leetcode(202): a new solution: two-pointers.

--- hash-table-easy/202.happy-number.go
+++ hash-table-easy/202.happy-number.go
@@ -9,25 +9,43 @@ import "fmt"
  */
 
 // @lc code=start
-func isHappy(n int) bool {
-	sum := n
-	counter := make(map[int]int)
-	for sum != 1 {
-		_, exists := counter[sum]
-		if exists {
-			break
-		}
-		counter[sum]++
-		n = sum
-		sum = 0
-		for n != 0 {
-			t := n % 10
-			n = n / 10
-			sum += t * t
-		}
+// func isHappy(n int) bool {
+// 	sum := n
+// 	counter := make(map[int]int)
+// 	for sum != 1 {
+// 		_, exists := counter[sum]
+// 		if exists {
+// 			break
+// 		}
+// 		counter[sum]++
+// 		n = sum
+// 		sum = 0
+// 		for n != 0 {
+// 			t := n % 10
+// 			n = n / 10
+// 			sum += t * t
+// 		}
 
+// 	}
+// 	return sum == 1
+// }
+
+func isHappy(n int) bool {
+	slow, fast := n, step(n)
+	for fast != 1 && slow != fast {
+		slow = step(slow)
+		fast = step(step(fast))
 	}
-	return sum == 1
+	return fast == 1
+}
+
+func step(n int) int {
+	sum := 0
+	for n > 0 {
+		sum += (n % 10) * (n % 10)
+		n = n / 10
+	}
+	return sum
 }
 
 // @lc code=end
