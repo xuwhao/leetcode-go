939f5ec7fe5d4e758391fcd6262bff71d50cbb3c
xuwhao <63779478@qq.com>
2022-03-17 23:58:12 +0800

    leetcode(876): restore in another place.

--- linked-list-easy/876.middle-of-the-linked-list.go
+++ linked-list-easy/876.middle-of-the-linked-list.go
@@ -1,14 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"leetcode-go/util"
+)
+
 /*
  * @lc app=leetcode id=876 lang=golang
  *
  * [876] Middle of the Linked List
  */
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
+// type ListNode struct {
+// 	Val  int
+// 	Next *ListNode
+// }
 
 // @lc code=start
 /**
@@ -31,3 +36,9 @@ func middleNode(head *ListNode) *ListNode {
 }
 
 // @lc code=end
+
+func testMiddleNode() {
+	head := []int{4, 2}
+	ans := util.List2Ints(middleNode(util.Ints2List(head)))
+	fmt.Println(ans)
+}
