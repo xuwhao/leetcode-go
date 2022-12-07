a0c089fa1c6ae63355b2ab51bc2fbc0cc84f3e79
xuwhao <63779478@qq.com>
2022-12-07 16:28:39 +0800

    leetcode(3): longest substring without repeating characters.

--- sliding-window-medium/3.longest-substring-without-repeating-characters.go
+++ sliding-window-medium/3.longest-substring-without-repeating-characters.go
@@ -66,31 +66,67 @@ import "fmt"
 
 // }
 
+// func lengthOfLongestSubstring(s string) int {
+// 	if len(s) == 0 {
+// 		return 0
+// 	}
+// 	var freq [256]int
+// 	result, left, right := 0, 0, -1
+
+// 	for left < len(s) {
+// 		if right+1 < len(s) && freq[s[right+1]-'a'] == 0 {
+// 			freq[s[right+1]-'a']++
+// 			right++
+// 		} else {
+// 			freq[s[left]-'a']--
+// 			left++
+// 		}
+// 		result = max(result, right-left+1)
+// 	}
+// 	return result
+// }
+
+// func max(a int, b int) int {
+// 	if a > b {
+// 		return a
+// 	}
+// 	return b
+// }
+
 func lengthOfLongestSubstring(s string) int {
 	if len(s) == 0 {
 		return 0
 	}
-	var freq [256]int
-	result, left, right := 0, 0, -1
+
+	ans := 1
+	left, right := 0, 1
+	flag := [256]int{0}
+
+	// s[0] 进入窗口，出现次数 + 1
+	flag[s[left]-'a']++
 
 	for left < len(s) {
-		if right+1 < len(s) && freq[s[right+1]-'a'] == 0 {
-			freq[s[right+1]-'a']++
-			right++
-		} else {
-			freq[s[left]-'a']--
-			left++
-		}
-		result = max(result, right-left+1)
-	}
-	return result
-}
 
-func max(a int, b int) int {
-	if a > b {
-		return a
+		if right == len(s) {
+			break
+		}
+
+		if flag[s[right]-'a'] != 0 {
+			flag[s[left]-'a']--
+			left++
+		} else {
+			flag[s[right]-'a']++
+			right++
+		}
+
+		if right-left > ans {
+			ans = right - left
+		}
+
 	}
-	return b
+
+	return ans
+
 }
 
 // @lc code=end
